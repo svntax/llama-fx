1c7af091cec8d22efbafb93112d0a13ae786856d
svntax <endless.answers@gmail.com>
2025-01-13 06:48:08 -0500

    Implemented launch functionality and error notifications.
    Pressing the Launch button now runs llama.cpp's llama-server application with the parameters set by the user. Error notifications will pop up if any occur during launch.

--- src/main/java/com/svntax/llamafx/MainController.java
+++ src/main/java/com/svntax/llamafx/MainController.java
@@ -1,23 +1,38 @@
 package com.svntax.llamafx;
 
+import atlantafx.base.controls.Notification;
+import atlantafx.base.theme.Styles;
+import atlantafx.base.util.Animations;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.util.converter.IntegerStringConverter;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainController {
+
+    @FXML
+    private StackPane stackPane;
 
     private StringProperty pathToLlamacppFolder;
     @FXML
@@ -106,8 +121,84 @@ public class MainController {
 
     }
 
+    private void showErrorNotification(String msg){
+        Notification errorNotification = new Notification(msg);
+        errorNotification.getStyleClass().addAll(Styles.ELEVATED_1, Styles.DANGER);
+        errorNotification.setPrefHeight(Region.USE_PREF_SIZE);
+        errorNotification.setMaxHeight(Region.USE_PREF_SIZE);
+        errorNotification.translateYProperty().set(-100);
+
+        errorNotification.setOnClose(e -> {
+            var out = Animations.slideOutUp(errorNotification, Duration.millis(250));
+            out.setOnFinished(f -> stackPane.getChildren().remove(errorNotification));
+            out.playFromStart();
+        });
+
+        StackPane.setAlignment(errorNotification, Pos.TOP_RIGHT);
+        StackPane.setMargin(errorNotification, new Insets(10, 10, 0, 0));
+        stackPane.getChildren().add(errorNotification);
+        errorNotification.widthProperty().addListener(((observable, oldValue, newValue) -> {
+            var in = Animations.slideInDown(errorNotification, Duration.millis(250));
+            in.playFromStart();
+        }));
+    }
+
     @FXML
     private void onLaunch(ActionEvent event){
+        List<String> commands = new LinkedList<String>();
+        ProcessBuilder builder = new ProcessBuilder();
+        String os = System.getProperty("os.name");
+        if (os.startsWith("Windows")) {
+            commands.add("cmd.exe");
+            commands.add("/c");
+            commands.add("start");
+        }
+        else{
+            commands.add("sh");
+            commands.add("-c");
+        }
 
+        String[] llamaCommands = getAllCommands();
+        commands.addAll(Arrays.asList(llamaCommands));
+        builder.command(commands);
+        File llamacppDir = new File(pathToLlamacppFolderField.getText());
+        File llamaServer = new File(pathToLlamacppFolderField.getText() + "/llama-server.exe");
+        if(!llamacppDir.exists() || !llamacppDir.isDirectory() || !llamaServer.exists()){
+            showErrorNotification("Error: Failed to find llama-server.exe\nCheck the llama.cpp path.");
+            return;
+        }
+        File model = new File(pathToModelField.getText());
+        if(!model.exists() || model.isDirectory()){
+            showErrorNotification("Error: Cannot find model file: " + pathToModelField.getText());
+            return;
+        }
+        builder.directory(llamacppDir);
+        Process process = null;
+        try {
+            process = builder.start();
+
+            int exitCode = process.waitFor();
+            if(exitCode != 0){
+                showErrorNotification("Error: Process ended with exit code " + exitCode);
+            }
+        } catch (IOException e) {
+            showErrorNotification("Error: " + e.getMessage());
+        } catch (InterruptedException e) {
+            showErrorNotification("Error: " + e.getMessage());
+        }
+    }
+
+    private String[] getAllCommands(){
+        String modelPath = pathToModelField.getText();
+        int gpuLayers = Integer.parseInt(gpuLayersField.getText());
+        gpuLayers = Math.max(0, gpuLayers);
+        int threads = Integer.parseInt(threadsField.getText());
+        threads = Math.max(1, threads);
+        int port = Integer.parseInt(portField.getText());
+        int contextSize = Integer.parseInt(contextSizeLabel.getText());
+        String[] llamaCommands = {"llama-server.exe", "-m", modelPath,
+                "--n-gpu-layers", Integer.toString(gpuLayers), "--threads", Integer.toString(threads),
+                "--ctx-size", Integer.toString(contextSize), "--port", Integer.toString(port)};
+        return llamaCommands;
     }
 }
